29bb1527f6c9d1203ea5f66f16efb738b585a934
elvinibbotson <elvinibbotson@outlook.com>
2018-01-04 17:15:45 +0000

    fixes  to delete code

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -1,6 +1,8 @@
 (function() {
-  // 'use strict';
-
+  'use strict';
+	
+  // global variables
+	
   var app = {
     isLoading: true,
     spinner: document.querySelector('.loader'),
@@ -8,28 +10,30 @@
     addDialog: document.getElementById('addDialog'),
 	editDialog: document.getElementById('editDialog'),
 	deleteDialog: document.getElementById('deleteDialog'),
-	alertDialog: document.getElementById('alertDialog'),
 	jottings: [],
 	jottingList: [],
 	jotting: null,
 	path: [],
 	list: document.getElementById('list'),
-	listName: 'Jottings',  
-	x:0,
-	y:0
+	listName: 'Jottings'
   };
 
-
-  /*****************************************************************************
-   *
-   * Event listeners for UI elements
-   *
-   ****************************************************************************/
-
+  // set up event listeners
+  
   document.getElementById("heading").addEventListener('click', function() {
   	console.log("edit heading");
 	if(app.path.length>0) { // no effect at top level
-		app.toggleDialog("editDialog", true); // show section neame in edit dialog
+		document.getElementById('butDelete').disabled=false;
+		document.getElementById('butDelete').style.color='red';
+		console.log("jotting content: "+app.jotting.content);
+		// try this...
+		console.log(app.jottingList.length+" items");
+		if(app.jottingList.length>0) { // cannot delete non-empty lists
+			document.getElementById('butDelete').disabled=true;
+			document.getElementById('butDelete').style.color='gray';
+			console.log("delete button disabled");
+		}
+		app.toggleDialog("editDialog", true); // show section name in edit dialog
 		document.getElementById('text').value=app.listName;
 		console.log("app.jotting is "+app.jotting.text);
 	}
@@ -43,12 +47,14 @@
 	if(app.path.length>0) {
 	 	var i=0;
 		while(i<app.path.length) {
-			list=list[app.path[i++]].content;
+			list=list[app.path[i++]];
 			console.log("list: "+list);
 		}
 		app.listName=list.text;
+		list=list.content;
 	 }
 	else app.listName="Jottings";
+	console.log("list name: "+app.listName);
 	app.jottingList=list;
 	app.populateList();
   });
@@ -74,18 +80,6 @@
 	console.log("selected type: "+type);
 	if(type>0) jotting.content=[];
 	app.jottingList.push(jotting);
-	/*
-	if(app.path.length<1) app.jottings.push(jotting); // insert into full jottings list...
-	else {
-		var j=app.jottings[app.path[0]];
-		var i=1;
-		while (i<app.path.length) {
-			j=j[app.path[i++]];
-		}
-		console.log('save jotting '+jotting.text+' in section '+j.text);
-		j.content.push(jotting);
-	}
-	*/
 	app.saveJottings(); // ...and save
 	app.populateList();
     app.toggleDialog('addDialog',false);
@@ -98,21 +92,10 @@
   
   document.getElementById('butDelete').addEventListener('click', function() {
   	// initiate delete jotting/list
-	  console.log("delete jotting "+app.jotting.text);
-	  document.getElementById('butDelete').disabled=false;
-	// app.jotting = app.jottingList[i];
-		if(app.jotting.content!=null) {
-			console.log(app.jotting.content.length+" items");
-			if(app.jotting.content.length>0) { // cannot delete lists unless empty
-				document.getElementById('butDelete').disabled=true;
-				console.log("delete button disabled");
-				// app.toggleDialog("alertDialog", true);
-				// return;
-			}
-		}
-		app.toggleDialog("deleteDialog", true);
-		document.getElementById('deleteText').innerHTML = app.jotting.text;
-		app.toggleDialog("editDialog", false);
+	console.log("delete jotting "+app.jotting.text);
+	app.toggleDialog("deleteDialog", true);
+	document.getElementById('deleteText').innerHTML = app.jotting.text;
+	app.toggleDialog("editDialog", false);
   });
   
   document.getElementById('butSave').addEventListener('click', function() {
@@ -122,15 +105,7 @@
 	  console.log("Save jotting");
 	  app.jotting.text = text;
 	  if(app.jotting.content!=null) app.listName=text;
-	  /*
-	  var j=app.jottings[app.path[0]]; // update in full jottings list...
-	  var i=1; 
-	  while (i<app.path.length) {
-			j=j[app.path[i++]];
-	  }
-	  j=app.jotting;
-	  */
-	  app.saveJottings(); // ...and save
+	  app.saveJottings();
 	  app.populateList();
   })
  
@@ -142,10 +117,45 @@
   document.getElementById('butDeleteConfirm').addEventListener('click', function() {
   	// confirm delete jotting/list
 	console.log("delete jotting "+app.jotting.text);
-	var i=app.jottingList.indexOf(app.jotting);
-	console.log("item "+i);
-	app.jottingList.splice(i,1);
-	app.populateList();
+	var list=app.jottings;
+	var i=0;  
+	if(app.jottingList.length<1) { // delete empty list (below top level) so...
+		console.log("path length: "+app.path.length);
+		app.path.pop(); // deleting this (empty) list
+		while(i<app.path.length) {
+			console.log("path["+i+"]: "+app.path[i]);
+			list=list[app.path[i++]];
+			console.log("list: "+list.text+" - first item: "+list.content[0].text);
+		}
+		app.jottingList=list.content; // ...jottingList is parent list
+		app.listName=list.text;
+		// test code
+		console.log("list: "+app.jottingList);
+		i=0;
+		while(i<app.jottingList.length) {
+			console.log("item "+i+": "+app.jottingList[i++].text);
+		}
+		i=app.jottingList.indexOf(app.jotting);
+		console.log("item "+i);
+		app.jottingList.splice(i,1);
+	}
+	else { // delete jotting
+		i=app.jottingList.indexOf(app.jotting);
+		console.log("item "+i);
+		app.jottingList.splice(i,1);
+		if(app.path.length>0) {
+			while(i<app.path.length) {
+				list=list[app.path[i++]];
+				console.log("list: "+list);
+			}
+			app.listName=list.text;
+			list=list.content;
+	 	}
+		else app.listName="Jottings";
+		console.log("list name: "+app.listName);
+		app.jottingList=list;
+		app.populateList();
+	}
 	app.saveJottings();
 	app.toggleDialog('deleteDialog', false);
   });
@@ -154,17 +164,8 @@
     // Close the delete dialog
     app.toggleDialog('deleteDialog', false);
   });
-  /* not needed
-   document.getElementById('butCancelAlert').addEventListener('click', function() {
-    // Close the alert dialog
-    app.toggleDialog('alertDialog', false);
-  });
- */
-   /*****************************************************************************
-   * Methods to update/refresh the UI
-   ****************************************************************************/
 
-  // Toggles the visibility of the add new jotting dialog.
+  // Toggles the visibility of dialogs
   app.toggleDialog = function(d, visible) {
 	  console.log("show "+d+" dialog: "+visible);
 	  if(d=='addDialog') { // toggle ADD dialog
@@ -189,13 +190,6 @@
      		 app.deleteDialog.classList.remove('dialog-container--visible');
     		}
 	  }
-	   else  { // toggle ALERT dialog
-	  	if (visible) {
-      		app.alertDialog.classList.add('dialog-container--visible');
-   		 } else {
-     		 app.alertDialog.classList.remove('dialog-container--visible');
-    		}
-	  }
   };
 
   // Save jottings to localStorage FOR NOW - LATER USE HOODIE
@@ -205,56 +199,27 @@
 	console.log("JOTTINGS SAVED: "+jottings);
   };
   
-  // monitor  tap/drag operations
-  app.touchStart = function(event) {
-  	var item=event.target;
-	app.x=event.clientX;
-	app.y=event.clientY;
-	console.log("touch at: "+app.x+","+app.y);
-  }
-  
-  app.touchEnd= function(event) {
-  	var item=event.target;
-	var x=event.clientX;
-	var y=event.clientY;
-	x-=app.x;
-	y-=app.y;
-	console.log("dragged: "+x+","+y);
+  // Open jotting/section
+  app.openItem = function() {
+	var item=event.target;
+	event.preventDefault();
 	var text = item.textContent;
 	var i=0;
 	while(app.jottingList[i].text!=text) {i++};
 	console.log("item "+i+": "+app.jottingList[i]);
-	if((Math.abs(x)<10)&&(Math.abs(y)<10)) app.openItem(i);
-	else if((Math.abs(y)<10)&&(x<-20)) {
-		console.log("DELETE"); // ---------- NEED TO WRITE DELETE CODE ----------------------
-		app.jotting = app.jottingList[i];
-		if(app.jotting.content!=null) {
-			if(app.jotting.content.length>0) { // cannot delete lists unless empty
-				app.toggleDialog("alertDialog", true);
-				return;
-			}
-		}
-		app.toggleDialog("deleteDialog", true);
-		document.getElementById('deleteText').innerHTML = app.jotting.text;
-		// }
-	}
-	else if((Math.abs(x)<10)&&(y<-20)) console.log("MOVE UP"); // ------- NEED TO WRITE MOVE UP/DOWN CODE ---------
-	else if((Math.abs(x)<10)&&(y>20)) console.log("MOVE DOWN");
-  }
-  
-  // Open jotting/section
-  app.openItem = function(i) {
-	  app.jotting = app.jottingList[i];
-	  if(app.jotting.content!=null) { // list
+	app.jotting = app.jottingList[i];
+	if(app.jotting.content!=null) { // list
 		  app.path.push(i); // path lists hierarchy to current section
 		  app.jottingList = app.jotting.content;
 		  app.listName=app.jotting.text;
 		  app.populateList();
-	  }
-	  else { // just a jotting - open for editing
+	}
+	else { // just a jotting - open for editing
+		document.getElementById('butDelete').disabled=false;
+		document.getElementById('butDelete').style.color='red';
 		app.toggleDialog("editDialog", true);
 		document.getElementById("text").value = app.jotting.text;
-	  }
+	}
   }
   
   app.populateList = function() {
@@ -264,9 +229,10 @@
   	for(var i in app.jottingList) {
   	 	var listItem = document.createElement('li');
 	  	listItem.classList.add('item');
-		// listItem.addEventListener('click', app.openItem, false);
-		listItem.addEventListener('pointerdown',app.touchStart,false);
-		listItem.addEventListener('pointerup',app.touchEnd,false);
+		listItem.addEventListener('click', app.openItem, false);
+		// listItem.addEventListener('pointerdown',app.touchStart,false);
+		// listItem.addEventListener('touchmove',app.drag,false);
+		// listItem.addEventListener('pointerup',app.touchEnd,false);
 		listItem.textContent = app.jottingList[i].text;
 		if(app.jottingList[i].content!=null) {
 			listItem.style.fontWeight='bold';
@@ -315,8 +281,6 @@
   }
   else {     // initial testing with fake data
 	  console.log("data:"+fakeData);
-	  // var data=JSON.parse(fakeData);
-	  // console.log("data:"+data);
 	  app.jottings = fakeData.jottings; // data.jottings
 	  console.log("jottings:"+app.jottings);
   }
@@ -324,41 +288,7 @@
   app.jottingList = app.jottings;
   app.listName="Jottings";
   app.populateList();
-  /*
-  app.list.children = null; // clear list
-  for(var i in app.jottingList) {
-  	   var listItem = document.createElement('li');
-	  listItem.classList.add('item');
-	  listItem.textContent = app.jottingList[i].name;
-	  listItem.addEventListener('click', app.openItem, false);
-	  if(app.jottingList[i].name!=null) listItem.style.fontWeight='bold';
-	  app.list.appendChild(listItem);
-  }
-  */
-  // app.toggleDialog("editDialog",true);
-  /*
-  var text="";
-  for(var i in app.list) {
-	  var content=app.list[i].content;
-	  console.log("content "+i+" size: "+content.length);
-	  console.log("first sub-item: "+content[0]);
-	  if(content[0].name!=null) {
-		  var n=content.length;
-		  console.log(n+" objects");
-	  }
-  	text+="<li class='item' ";
-	   if(content[0].name!=null) {
-		   text += "style='font-weight:bold' "
-	   }
-	   text+=">"+app.list[i].name+"</li>";
-  }
-  text+="</ul>";
-  document.getElementById('list').innerHTML=text;
-  });
-  */
-
-  
-  
+    
   if (app.isLoading) {
       app.spinner.setAttribute('hidden', true);
       app.container.removeAttribute('hidden');
